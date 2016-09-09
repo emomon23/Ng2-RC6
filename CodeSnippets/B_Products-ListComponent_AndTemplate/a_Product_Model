071083bf2d3b9ff3a2edb56e821823c09f2f74d1
emomon23 <memo@iemosoft.com>
2016-09-09 08:54:46 -0500

    Code Snippets

---
+++ CodeSnippets/B_Products-ListComponent_AndTemplate/a_Product_Model
@@ -0,0 +1,8 @@
+export class Product{
+    productId: number
+    name: string;
+    category: string;
+    description: string;
+    price: number;
+    rating: number
+}
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,27 +1,7 @@
 import { Component } from '@angular/core';
+
 @Component({
   selector: 'app-component',
-  template: `<div class='navbar navbar-inverse'>
-        <h1>
-	        <a class='nav-brand' href='#'>Sportz Ur Way</a>
-         </h1>
-    </div>
-
-    <div class="panel panel-default row">
-        <div class="col-xs-3">
-            <a class='btn btn-primary btn-block btn-default btn-lg'>Home</a>
-        </div>
-        <div class="col-xs-8">
-            <div class="well">
-                <h3>
-                    <strong>Product Name</strong>
-                    <span class="pull-right label label-primary">
-                       $100.95
-                    </span>
-                </h3>
-                <span class="lead">The best product EVER!</span>
-            </div>
-        </div>
-    </div>`
+  template: `Hello MDC!`,
 })
 export class AppComponent { }
