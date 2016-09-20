ecd7cbbe211f4e7bce0835e59095f13757a34075
emomon23 <memo@iemosoft.com>
2016-09-19 20:36:43 -0500

    Products component callign Http products.json directly

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,10 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
 import { ProductListComponent } from './products/product-list.component'
-
+import { HttpModule } from '@angular/http'
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, HttpModule ],
 
   //Declare Components, NOT SERVICES!!
   declarations: [ AppComponent, ProductListComponent ],
--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -1,8 +1,28 @@
 import { Component } from '@angular/core';
 import { Product } from './product'
+import { Headers, Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import  'rxjs/add/operator/map'; 
+
 
 @Component({
   selector: 'product-list',
   templateUrl: './app/products/product-list.component.html'
 })
-export class ProductListComponent { }
+export class ProductListComponent {
+
+    private _producttUrl =  'api/products/products.json';
+
+    products : Product[] = []
+
+    constructor(private _http:Http){
+        this.getProducts().subscribe(p => this.products = p);
+    }
+
+    getProducts() : Observable<Product[]> {
+        return this._http.get(this._producttUrl)
+                   .map((response: Response) => <Product[]>response.json());
+    }
+
+
+ }
